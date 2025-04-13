81dd1cfac0db862e72147950c97353da9f496b4b
itachi880 <142436626+itachi880@users.noreply.github.com>
2025-04-13 16:05:28 +0100

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/test/java/com/itachi880/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itachi880.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
